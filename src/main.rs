9366df244bab439296a31380923c8f37b2cd4da6
Pavel Perestoronin <pavel.perestoronin@kpn.com>
2023-05-26 01:24:45 +0200

    🔧 So much for WASI, switch back to the default targets

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+mod prelude;
+
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+
+use axum::routing::get;
+use axum::Router;
+
+use crate::prelude::*;
+
+#[tokio::main]
+async fn main() -> Result {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::LOCALHOST, 8080)))
+        .serve(app.into_make_service())
+        .await
+        .context("the server has failed")
 }
---
+++ src/prelude.rs
@@ -0,0 +1,2 @@
+pub type Result<T = ()> = anyhow::Result<T>;
+pub use anyhow::Context;
