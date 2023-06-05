476664abe66122173357c2540151388af8e4b3a0
Pavel Perestoronin <pavel.perestoronin@kpn.com>
2023-06-06 00:37:36 +0200

    🎨 Move the static files to their own directory

--- src/web/static.rs
+++ src/web/static.rs
@@ -1,3 +1,24 @@
+//! ```text
+//! Add this to your HTML <head>:
+//!
+//!     <link rel="icon" href="/favicon.ico" sizes="any">
+//!     <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+//!
+//! Add this to your app's manifest.json:
+//!
+//!     ...
+//!     {
+//!       "icons": [
+//!         { "src": "/favicon.ico", "type": "image/x-icon", "sizes": "16x16 32x32" },
+//!         { "src": "/icon-192.png", "type": "image/png", "sizes": "192x192" },
+//!         { "src": "/icon-512.png", "type": "image/png", "sizes": "512x512" },
+//!         { "src": "/icon-192-maskable.png", "type": "image/png", "sizes": "192x192", "purpose": "maskable" },
+//!         { "src": "/icon-512-maskable.png", "type": "image/png", "sizes": "512x512", "purpose": "maskable" }
+//!       ]
+//!     }
+//!     ...
+//! ```
+
 use crate::web::prelude::*;
 
 pub async fn get_favicon() -> impl IntoResponse {
@@ -6,27 +27,27 @@ pub async fn get_favicon() -> impl IntoResponse {
             Headers::CONTENT_TYPE_MICROSOFT_ICON,
             Headers::CACHE_PUBLIC_WEEK,
         ],
-        include_bytes!("favicon.ico"),
+        include_bytes!("static/favicon.ico"),
     )
 }
 
 pub async fn get_apple_touch_icon() -> impl IntoResponse {
     (
         [Headers::CONTENT_TYPE_PNG, Headers::CACHE_PUBLIC_WEEK],
-        include_bytes!("apple-touch-icon.png"),
+        include_bytes!("static/apple-touch-icon.png"),
     )
 }
 
 pub async fn get_icon_192() -> impl IntoResponse {
     (
         [Headers::CONTENT_TYPE_PNG, Headers::CACHE_PUBLIC_WEEK],
-        include_bytes!("icon-192.png"),
+        include_bytes!("static/icon-192.png"),
     )
 }
 
 pub async fn get_icon_512() -> impl IntoResponse {
     (
         [Headers::CONTENT_TYPE_PNG, Headers::CACHE_PUBLIC_WEEK],
-        include_bytes!("icon-512.png"),
+        include_bytes!("static/icon-512.png"),
     )
 }
