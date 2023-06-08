8dc201236db8fcc83a9aad7f170abae8caf479c0
Pavel Perestoronin <eigenein@gmail.com>
2023-06-09 00:34:37 +0200

    🔊 Instrument `index::get()`

--- src/web/index.rs
+++ src/web/index.rs
@@ -2,10 +2,12 @@
 
 use axum::extract::State;
 use maud::{html, Markup};
+use tracing::instrument;
 
 use crate::web::{partials::*, state::*};
 
 /// Index route handler.
+#[instrument(skip_all)]
 pub async fn get(State(state): State<AppState>) -> Markup {
     html! {
         (head())
