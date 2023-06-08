dfc747f4848278a1a679df6472ec0edff49c3ed7
Pavel Perestoronin <pavel.perestoronin@kpn.com>
2023-06-08 17:26:54 +0200

    🚧 Add the vehicles' stats cache

--- src/web/state.rs
+++ src/web/state.rs
@@ -1,8 +1,13 @@
-use std::sync::Arc;
+use std::{collections::hash_map::RandomState, sync::Arc, time::Duration};
+
+use moka::future::Cache;
 
 #[cfg(test)]
 use crate::prelude::Result;
-use crate::{db::Db, weegee::WeeGee};
+use crate::{
+    db::Db,
+    weegee::{VehiclesStats, WeeGee},
+};
 
 #[derive(Clone)]
 pub struct AppState {
@@ -12,7 +17,11 @@ pub struct AppState {
     pub sign_in_url: Arc<String>,
 
     pub db: Db,
-    pub wg: WeeGee,
+    weegee: WeeGee,
+
+    /// Account's vehicle's cache,
+    /// used to check whether a certain user is allowed to rate a certain vehicle.
+    vehicles_stats_cache: Cache<u32, Arc<VehiclesStats>, RandomState>,
 }
 
 impl AppState {
@@ -24,10 +33,13 @@ impl AppState {
     ) -> Result<Self> {
         Ok(Self {
             db,
-            wg: WeeGee::new(backend_application_id)?,
+            weegee: WeeGee::new(backend_application_id)?,
             sign_in_url: Arc::new(format!(
                 "https://api.worldoftanks.eu/wot/auth/login/?application_id={frontend_application_id}&redirect_uri=//{domain_name}/authenticate"
             )),
+            vehicles_stats_cache: Cache::builder()
+                .time_to_idle(Duration::from_secs(600))
+                .build(),
         })
     }
 
